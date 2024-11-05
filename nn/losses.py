a118121f4f8b8ed42897d8740c95419ba59e3f8a
basic-bgnr <devkotabidit@gmail.com>
2024-11-05 14:21:08 +0545

    changed summation axis so that its generic over all shape of output

--- nn/losses.py
+++ nn/losses.py
@@ -7,9 +7,9 @@ class MSE:
         Here axis=(rank-2, rank-1) is taken so that sum is calculated across x,y plane (works for both 3d/2d)
         """
         rank = np.ndim(y_true)
-        axis = (
-            (rank - 2, rank - 1) if rank >= 2 else (0,)
-        )  # sum across plane(row, column)
+        axis = tuple(
+            range(1, rank)
+        )  # sum across all axis except the first (batch dimension)
         return np.sum(np.power(y_true - y_pred, 2), axis=axis)
 
     def loss_prime(self, y_true, y_pred):
@@ -22,9 +22,9 @@ class CrossEntropy:
         Here axis=(rank-2, rank-1) is taken so that sum is calculated across x,y plane (works for both 3d/2d)
         """
         rank = np.ndim(y_true)
-        axis = (
-            (rank - 2, rank - 1) if rank >= 2 else (0,)
-        )  # sum across plane(row, column)
+        axis = tuple(
+            range(1, rank)
+        )  # sum across all axis except the first (batch dimension)
         return -np.sum(
             y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred), axis=axis
         )
